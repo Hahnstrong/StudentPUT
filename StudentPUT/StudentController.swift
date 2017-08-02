734460928bdeedbcb88bef6b960b59da68af9605
Caleb Strong <hahnstrong@gmail.com>
2017-08-02 12:27:20 -0600

    Add fetchStudents to StudentController and UI to Main.storyboard

--- StudentPUT/StudentController.swift
+++ StudentPUT/StudentController.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+class StudentController {
+    
+    // DataSource
+    // Build URL
+    // Fetch Students
+    // Post Students
+    
+    static var students: [Student] = []
+    
+    static let baseURL = URL(string: "https://survey-ios14.firebaseio.com/students")
+    
+    static func fetchStudents(completion: @escaping () -> Void) {
+        
+        guard let url = baseURL?.appendingPathExtension("json") else { completion(); return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.httpBody = nil
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                NSLog("There was an error: \(error.localizedDescription)")
+                completion()
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from the dataTask")
+                completion()
+                return
+            }
+            
+            guard let studentsDictionary = ( try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: [String: String]] else {
+                NSLog("Unable to serialize JSON")
+                completion()
+                return
+            }
+            
+            students = studentsDictionary.flatMap { Student(dictionary: $0.value) }
+            completion()
+        }
+        
+        dataTask.resume()
+    }
+    
+}
--- StudentPUT/StudentListTableViewController.swift
+++ StudentPUT/StudentListTableViewController.swift
@@ -21,7 +21,7 @@ class StudentListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
 
         return cell
     }
